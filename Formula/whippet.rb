c42c6b49a7fdcdc02fe2e5fc291885e5667dc318
Rob Skilling <rob@dxw.com>
2022-03-30 10:43:03 +0100

    Use Whippet v2.2.1

--- Formula/whippet.rb
+++ Formula/whippet.rb
@@ -1,8 +1,8 @@
 class Whippet < Formula
   desc "Framework for building WordPress sites"
   homepage "https://github.com/dxw/whippet"
-  url "https://github.com/dxw/whippet/archive/v2.2.0.tar.gz"
-  sha256 "9c3c02dd9c854ec1b0affbed906d642ca49d22a94762f2fe110ac38f21be459b"
+  url "https://github.com/dxw/whippet/archive/v2.2.1.tar.gz"
+  sha256 "41d9193fca902521198a2fdf8e78e0bc3f8107388c8cdc3995029836ea6f5f80"
 
   depends_on "composer" => :build
   depends_on "subversion" => :build
